21c82503e8687b3c0df5bb24ca2533d5b56ccb2d
greeny3ny <craig-little@live.co.uk>
2019-07-25 23:02:20 +0100

    Add files via upload

---
+++ scripts/api_keys.js
@@ -0,0 +1,7 @@
+//I know this exposes the API keys used, but it's just a fun project so should not be a huge issue.
+
+var api_keys = {
+
+	FIREBASE_KEY : 'AIzaSyA6pk5vRkVWlQRXKOjLJ_khMUMvGnTzlkQ'
+
+}
